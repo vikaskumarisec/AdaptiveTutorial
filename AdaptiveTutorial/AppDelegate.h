455f04cee1296d5019b1ff63230ef26faa478db5
maharaj <maharaj@web18mac000003.local>
2018-05-14 17:58:49 +0530

    Initial Commit

---
+++ AdaptiveTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdaptiveTutorial
+//
+//  Created by maharaj on 14/05/18.
+//  Copyright © 2018 maharaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdaptiveTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdaptiveTutorial
+//
+//  Created by maharaj on 14/05/18.
+//  Copyright © 2018 maharaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
